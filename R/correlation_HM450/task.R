3ff9e2b3c09e1c0a9da3204b3db0fe8183379db7
Mamie Wang <szmamie@stanford.edu>
2018-03-07 11:26:17 -0800

    Update the sbatch script to latest function defined

--- R/correlation_HM450/task.R
+++ R/correlation_HM450/task.R
@@ -1,7 +1,7 @@
 # File name: task.R
 # Author: Mamie Wang
 # Date created: 02/28/2018
-# Date modified: 02/28/2018
+# Date modified: 03/07/2018
 # Input: processed RNAseq and methylation data
 # Output: correlation coefficients between matched pairs of RNAseq and methylation probes
 
@@ -9,5 +9,8 @@ source('computeCorrelation.R')
 load('../../data/processed/RNAseq.RData')
 load('../../data/processed/HM450.RData')
 
-correlation <- ComputeCorrelation(RNAseq, data.HM450)
-save(correlation, file='../../data/processed/Correlation.RData')
+correlation <- ComputeCorrelation(RNAseq, data.HM450, T, NULL)
+save(correlation, file='../../data/processed/CorrelationM.RData')
+correlation <- ComputeCorrelation(RNAseq, data.HM450, F, NULL)
+save(correlation, file='../../data/processed/CorrelationB.RData')
+
